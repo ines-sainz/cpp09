358b49f127863a2bb9bf1941816ee9a524db9f39
Ines Sainz Ramon <isainz-r@c1r10s4.42madrid.com>
2025-03-03 10:42:27 +0100

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -14,7 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	if (argc != 2)
+	{
+		std::cout << "Error" << std::endl;
+		return (1);
+	}
 	if (pmerge_me(argv) == 1)
 		return (1);
 	return (0);
